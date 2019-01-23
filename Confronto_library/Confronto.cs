8963750d95ccd2508f0d72d0744a4fa641561434
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-01-23 08:55:44 +0100

    primo test red

--- Confronto_library/Confronto.cs
+++ Confronto_library/Confronto.cs
@@ -4,7 +4,10 @@ namespace Confronto_library
 {
     public class Confronto
     {
-
+        public static int Max(int n1, int n2)
+        {
+            return 0;
+        }
 
     }
 }
---
+++ Confronto_library_tests/Confronto_tests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Confronto_library_tests;
+
+namespace Confronto_library_tests
+{
+    [TestClass]
+    public class Confronto_tests
+    {
+        [TestMethod]
+        public void TestMax()
+        {
+            int n1 = 0;
+            int n2 = 7;
+            int max_aspettato = 7;
+
+            int max_calcolato = Confronto_library.Confronto.Max(n1, n2);
+            Assert.AreEqual(max_aspettato, max_calcolato);
+        }
+
+    }
+}
